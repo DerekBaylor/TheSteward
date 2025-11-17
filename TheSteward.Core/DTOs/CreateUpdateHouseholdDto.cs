1fe4ab81367bcf8448d559baddbbaedfab99809d
Derek B. <83030603+DerekBaylor@users.noreply.github.com>
2025-11-17 08:26:37 -0500

    Update Household page to show, create, and update a user household

---
+++ TheSteward.Core/DTOs/CreateUpdateHouseholdDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheSteward.Core.DTOs;
+
+public class CreateUpdateHouseholdDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string HouseholdName { get; set; } = string.Empty;
+
+    public bool HasTaskManagerAccess { get; set; }
+    public bool HasFinanceManagerAccess { get; set; }
+    public bool HasMealManagerAccess { get; set; }
+    public bool HasFileManagerAccess { get; set; }
+}

eb9ae76e0a185094f2f6b2f4d03eb603dc4356be
Derek B. <83030603+DerekBaylor@users.noreply.github.com>
2025-11-21 09:10:28 -0500

    Added AutoMapper nuget and Household profiles

---
+++ TheSteward.Core/Profiles/HouseholdProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TheSteward.Core.Profiles;
+
+public class HouseholdProfiles : Profile
+{
+    public HouseholdProfiles()
+    {
+        CreateMap<Models.Household, DTOs.HouseholdDto>().ReverseMap();
+        CreateMap<Models.Household, DTOs.CreateUpdateHouseholdDto>().ReverseMap();
+    }
+}

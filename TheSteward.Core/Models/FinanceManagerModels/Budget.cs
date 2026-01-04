e05f81f1910e93bdff10c0c500a360e548de03b9
Derek B. <83030603+DerekBaylor@users.noreply.github.com>
2026-01-03 20:13:13 -0500

    Budget entity started

---
+++ TheSteward.Core/Models/FinanceManagerModels/Budget.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TheSteward.Core.Models.HouseholdModels;
+
+namespace TheSteward.Core.Models.FinanceManagerModels;
+
+public class Budget
+{
+    [Key]
+    public Guid BudgetId { get; set; }
+    
+    [Required]
+    public required string Name { get; set; }
+
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    
+    public DateTime? ModifiedDate { get; set; }
+
+    public bool IsDefaultBudget { get; set; }
+
+    public bool IsPublic { get; set; }
+
+    [Required]
+    public required string OwnerId { get; set; }
+
+    #region Navigational Properties
+    public Guid HouseholdId { get; set; }
+    
+    [ForeignKey("HouseholdId")]
+    public required Household Household { get; set; }
+
+    #endregion Navigational Properties
+}
